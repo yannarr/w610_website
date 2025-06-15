5c4ed7be3d00c7677169fb687fcc5f1e9113b4ab
Mark Mayana <markogem4@gmail.com>
2025-06-15 15:39:17 +0300

    1

--- products.js
+++ products.js
@@ -3,133 +3,90 @@ const products = [
         id: 1,
         title: "Shea Butter",
         price: "ksh 1750",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        description: "Rich in vitamins A, E, and F, this luxurious butter deeply nourishes and hydrates your skin. Perfect for soothing dryness, irritation, and improving skin texture. Say goodbye to scars, stretch marks, and fine lines.",
         colors: [
             {
-                name: "Green",
+                name: "Blue",
                 mainImage: "assets/img/cart-2.png",
                 thumbnails: [
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
+                    "assets/img/cart-2.png",                 
                 ],
-                sizes: ["S", "M"]
-            },
-            {
-                name: "Yellow",
-                mainImage: "assets/img/cart-2.png",
-                thumbnails: [
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
-                ],
-                sizes: ["S", "M", "L", "XL", "XXL"]
-            },
-            {
-                name: "Red",
-                mainImage: "assets/img/cart-2.png",
-                thumbnails: [
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
-                ],
-                sizes: ["S", "M", "L", "XL", "XXL"]
-            },
-            {
-                name: "White",
-                mainImage: "assets/img/cart-2.png",
-                thumbnails: [
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
-                ],
-                sizes: ["S", "L"]
-            },
-            {
-                name: "Maroon",
-                mainImage: "assets/img/cart-2.png",
-                thumbnails: [
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
-                ],
-                sizes: ["M", "L", "XL"]
+                sizes: ["M", "L"]
             }
         ]
     },
+
     {
         id: 2,
-        title: "Men Slim Fit Turtleneck Sweater",
-        price: "$50",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        title: "Lavender Bouqet",
+        price: "Ksh 1950/=",
+        description: "A calming floral blend that soothes the senses and brings peace to your space. Lavender Bouquet is your go-to for relaxation, promoting calm vibes and stress relief after a long day.",
         colors: [
             {
                 name: "Blue",
                 mainImage: "assets/img/cart-2.png",
                 thumbnails: [
                     "assets/img/cart-2.png",
-                    "assets/img/cart-2.png",
-                    "assets/img/cart-2.png"
                 ],
-                sizes: ["M", "L", "XL"]
+                sizes: ["M", "L"]
             }
         ]
     },
     {
         id: 3,
-        title: "Men Striped Polo Shirt",
-        price: "$20",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        title: "Spicy Vanilla",
+        price: "Ksh 1850/=",
+        description: "Warm, inviting, and a little mysterious — Spicy Vanilla wraps you in cozy comfort. A sweet and spicy aroma perfect for setting the mood, whether it’s self-care or date night.",
         colors: [
             {
                 name: "Grey",
                 mainImage: "assets/img/cart-2.png",
-                thumbnails: ["assets/img/cart-2.png", "assets/img/cart-2.png", "assets/img/cart-2.png"],
-                sizes: ["S", "M", "L", "XL", "XXL"]
+                thumbnails: ["assets/img/cart-2.png"],
+                sizes: ["S", "L"]
             }
         ]
     },
     {
         id: 4,
-        title: "Men Casual Long Sleeve Polo",
-        price: "$125",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        title: "Tea Tree",
+        price: "Ksh 1950/=",
+        description: "Fresh, earthy, and purifying — Tea Tree delivers a crisp aroma that awakens your senses and clarifies your space. Ideal for a natural, clean vibe that leaves you feeling refreshed.",
         colors: [
             {
                 name: "Green",
                 mainImage: "assets/img/cart-2.png",
-                thumbnails: ["assets/img/cart-2.png", "assets/img/cart-2.png", "assets/img/cart-2.png"],
-                sizes: ["S", "M", "L", "XL", "XXL"]
+                thumbnails: ["assets/img/cart-2.png"],
+                sizes: ["S", "L"]
             }
         ]
     },
     {
         id: 5,
-        title: "Men Western Style Denim Shirt",
-        price: "$147",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        title: "Rose Bloom",
+        price: "Ksh 1950/=",
+        description: "Soft, romantic, and timeless — Rose Bloom captures the delicate beauty of blooming petals. A gentle floral fragrance that inspires love, grace, and elegance in every breath.",
         colors: [
             {
                 name: "Black",
                 mainImage: "assets/img/cart-2.png",
-                thumbnails: ["assets/img/cart-2.png", "assets/img/cart-2.png", "assets/img/cart-2.png"],
-                sizes: ["S", "M", "L", "XL", "XXL"]
+                thumbnails: ["assets/img/cart-2.png"],
+                sizes: ["S", "L"]
             }
         ]
     },
     {
         id: 6,
-        title: "Men Casual USA Print Tee",
-        price: "$80",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum aut explicabo molestias voluptas, eum nulla doloremque laboriosam. Accusantium omnis illum nulla architecto doloribus.",
+        title: "Lemon Zest",
+        price: "Ksh 1950/=",
+        description: "Bright, juicy, and uplifting — Lemon Zest is your daily dose of sunshine. Its citrusy kick energizes your space and mood, giving you a fresh start every time you use it.",
         colors: [
             {
                 name: "Brown",
                 mainImage: "assets/img/cart-2.png",
-                thumbnails: ["assets/img/cart-2.png", "assets/img/cart-2.png", "assets/img/cart-2.png"],
-                sizes: ["S", "M", "L", "XL", "XXL"]
+                thumbnails: ["assets/img/cart-2.png"],
+                sizes: ["S", "L"]
             }
         ]
     },
- 
+
 ];
